30d2acf2c0e46149c27d8411452e4f8bd4969972
BTCPay Plugin Developer <developer@btcpay.com>
2025-12-21 09:13:08 -0800

    Add BTCPay Server 2.3 compatibility
    - Add JSON manifest file required by BTCPay Server 2.3 plugin system
    - Update BTCPayServer dependency from >=2.0.0 to >=2.3.0
    - Configure build to include manifest file in output
    - Update plugin description to include Square integration

--- Plugins/BTCPayServer.Plugins.BitcoinRewards/BitcoinRewardsPlugin.cs
+++ Plugins/BTCPayServer.Plugins.BitcoinRewards/BitcoinRewardsPlugin.cs
@@ -16,7 +16,7 @@ public class BitcoinRewardsPlugin : BaseBTCPayServerPlugin
 
     public override IBTCPayServerPlugin.PluginDependency[] Dependencies { get; } =
     {
-        new IBTCPayServerPlugin.PluginDependency { Identifier = nameof(BTCPayServer), Condition = ">=2.0.0" }
+        new IBTCPayServerPlugin.PluginDependency { Identifier = nameof(BTCPayServer), Condition = ">=2.3.0" }
     };
 
 
