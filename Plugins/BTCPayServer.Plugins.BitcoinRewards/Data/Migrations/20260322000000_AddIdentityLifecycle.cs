2404f4b8297419ad1409e5a9b16c4202705acbf6
JP Gaviria <hello@trailscoffee.com>
2026-03-22 12:05:47 -0700

    Fix migration: add DbContext/Migration attributes, schema for ReleasedAt column

--- Plugins/BTCPayServer.Plugins.BitcoinRewards/Data/Migrations/20260322000000_AddIdentityLifecycle.cs
+++ Plugins/BTCPayServer.Plugins.BitcoinRewards/Data/Migrations/20260322000000_AddIdentityLifecycle.cs
@@ -1,16 +1,23 @@
 #nullable enable
 using System;
+using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
 
 namespace BTCPayServer.Plugins.BitcoinRewards.Data.Migrations;
 
+[DbContext(typeof(BitcoinRewardsPluginDbContext))]
+[Migration("20260322_AddIdentityLifecycle")]
 public partial class AddIdentityLifecycle : Migration
 {
+    private const string Schema = "BTCPayServer.Plugins.BitcoinRewards";
+
     protected override void Up(MigrationBuilder migrationBuilder)
     {
         migrationBuilder.AddColumn<DateTime>(
             name: "ReleasedAt",
-            schema: "BTCPayServer.Plugins.BitcoinRewards",
+            schema: Schema,
             table: "Nip05Identities",
             type: "timestamp with time zone",
             nullable: true);
@@ -20,7 +27,7 @@ public partial class AddIdentityLifecycle : Migration
     {
         migrationBuilder.DropColumn(
             name: "ReleasedAt",
-            schema: "BTCPayServer.Plugins.BitcoinRewards",
+            schema: Schema,
             table: "Nip05Identities");
     }
 }
